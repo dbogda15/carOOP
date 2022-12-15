9ea18b096ad737c724b2be2cdfa275c1eee45730
dianabogdanova <dbogda15@yandex.ru>
2022-12-15 20:51:20 +0500

    "car" part of the updated homework

--- src/Car.java
+++ src/Car.java
@@ -1,20 +1,13 @@
+import java.util.Objects;
+
 public class Car {
-    final String brand;
-    final String model;
+    String brand;
+    String model;
     double engineVolume;
     String color;
     // я подразумеваю, что цвет авто и объём двигателя в целом когда-то можно будет поменять)))
-    final int year;
-    final String country;
-
-    public Car (String brand, String model, double engineVolume, String color, int year, String country) {
-    this.brand = brand;
-    this.model = model;
-    this.engineVolume = engineVolume;
-    this.color = color;
-    this.year = year;
-    this.country = country;
-    }
+    int year;
+    String country;
 
     public String getBrand() {
         return brand;
@@ -52,4 +45,44 @@ public class Car {
     public String toString() {
         return brand + " " + model + ", " + year + " года выпуска, страна сборки: " + country + ", цвет кузова: " + color + ", объём двигателя -  " + engineVolume + " л.";
     }
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+
+        if (Objects.isNull(brand) || Objects.equals(brand, "") || Objects.equals(brand, " ")){
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+
+        if (Objects.isNull(model) || Objects.equals(model, "") || Objects.equals(model, " ")){
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+
+        if (engineVolume <=0 ) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+
+        if (Objects.isNull(color) || Objects.equals(color, "") || Objects.equals(color, " ")) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+
+        if (year <= 0 ) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
+
+        if (Objects.isNull(country) || Objects.equals(country, "") || Objects.equals(country, " ")) {
+            this.country = "default";
+        } else {
+            this.country = country;
+        }
+    }
 }
+
